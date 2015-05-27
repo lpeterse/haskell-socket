0b49a63dc7bc91bbc06bd66366e1cea834e746a9
Lars Petersen <lars@nyantec.com>
2015-05-27 14:07:09 +0200

    Moved FFI binding to seperate module.

--- src/System/Socket/Internal.hs
+++ src/System/Socket/Internal.hs
@@ -1,5 +1,8 @@
-module System.Socket.Internal
-  ( Socket (..)
+module System.Socket.Internal (
+  -- * Socket
+    Socket (..)
+  -- * SocketException
+  , SocketException (..)
   -- * FFI
   , c_socket
   , c_bind
@@ -12,8 +15,6 @@ module System.Socket.Internal
   , c_recvfrom
   , c_close
   , c_getsockopt
-  -- * SocketException
-  , SocketException (..)
   ) where
 
 import Control.Concurrent.MVar
@@ -23,6 +24,7 @@ import Foreign.C.Types
 
 import System.Posix.Types ( Fd(..) )
 
+import System.Socket.Internal.FFI
 import System.Socket.Internal.Exception
 
 -- | A generic socket type. Also see `socket` for details.
@@ -49,40 +51,3 @@ import System.Socket.Internal.Exception
 --     thread and read the library code to see how the problem is currently circumvented.
 newtype Socket d t p
       = Socket (MVar Fd)
-
--------------------------------------------------------------------------------
--- FFI
--------------------------------------------------------------------------------
-
-foreign import ccall unsafe "sys/socket.h socket"
-  c_socket  :: CInt -> CInt -> CInt -> IO Fd
-
-foreign import ccall unsafe "unistd.h close"
-  c_close   :: Fd -> IO CInt
-
-foreign import ccall unsafe "sys/socket.h bind"
-  c_bind    :: Fd -> Ptr a -> Int -> IO CInt
-
-foreign import ccall unsafe "sys/socket.h connect"
-  c_connect :: Fd -> Ptr a -> Int -> IO CInt
-
-foreign import ccall unsafe "sys/socket.h accept"
-  c_accept  :: Fd -> Ptr a -> Ptr Int -> IO Fd
-
-foreign import ccall unsafe "sys/socket.h listen"
-  c_listen  :: Fd -> Int -> IO CInt
-
-foreign import ccall unsafe "sys/socket.h send"
-  c_send    :: Fd -> Ptr CChar -> Int -> Int -> IO Int
-
-foreign import ccall unsafe "sys/socket.h sendto"
-  c_sendto  :: Fd -> Ptr CChar -> Int -> Int -> Ptr CChar -> Int -> IO Int
-
-foreign import ccall unsafe "sys/socket.h recv"
-  c_recv    :: Fd -> Ptr CChar -> Int -> Int -> IO Int
-
-foreign import ccall unsafe "sys/socket.h recvfrom"
-  c_recvfrom :: Fd -> Ptr CChar -> Int -> Int -> Ptr CChar -> Ptr Int -> IO Int
-
-foreign import ccall unsafe "sys/socket.h getsockopt"
-  c_getsockopt  :: Fd -> CInt -> CInt -> Ptr a -> Ptr Int -> IO CInt
---
+++ src/System/Socket/Internal/FFI.hs
@@ -0,0 +1,39 @@
+module System.Socket.Internal.FFI where
+
+import Foreign.Ptr
+import Foreign.C.Types
+
+import System.Posix.Types ( Fd(..) )
+
+foreign import ccall unsafe "sys/socket.h socket"
+  c_socket  :: CInt -> CInt -> CInt -> IO Fd
+
+foreign import ccall unsafe "unistd.h close"
+  c_close   :: Fd -> IO CInt
+
+foreign import ccall unsafe "sys/socket.h bind"
+  c_bind    :: Fd -> Ptr a -> Int -> IO CInt
+
+foreign import ccall unsafe "sys/socket.h connect"
+  c_connect :: Fd -> Ptr a -> Int -> IO CInt
+
+foreign import ccall unsafe "sys/socket.h accept"
+  c_accept  :: Fd -> Ptr a -> Ptr Int -> IO Fd
+
+foreign import ccall unsafe "sys/socket.h listen"
+  c_listen  :: Fd -> Int -> IO CInt
+
+foreign import ccall unsafe "sys/socket.h send"
+  c_send    :: Fd -> Ptr CChar -> Int -> Int -> IO Int
+
+foreign import ccall unsafe "sys/socket.h sendto"
+  c_sendto  :: Fd -> Ptr CChar -> Int -> Int -> Ptr CChar -> Int -> IO Int
+
+foreign import ccall unsafe "sys/socket.h recv"
+  c_recv    :: Fd -> Ptr CChar -> Int -> Int -> IO Int
+
+foreign import ccall unsafe "sys/socket.h recvfrom"
+  c_recvfrom :: Fd -> Ptr CChar -> Int -> Int -> Ptr CChar -> Ptr Int -> IO Int
+
+foreign import ccall unsafe "sys/socket.h getsockopt"
+  c_getsockopt  :: Fd -> CInt -> CInt -> Ptr a -> Ptr Int -> IO CInt
