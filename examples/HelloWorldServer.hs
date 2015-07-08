60ca2c9dc20cb598117ca61a2192a52244b8f1b0
Lars Petersen <lars@nyantec.com>
2015-07-08 21:10:22 +0200

    Updated example code.

--- examples/HelloWorldServer.hs
+++ examples/HelloWorldServer.hs
@@ -2,31 +2,22 @@
 module Main where
 
 import System.Socket
-import System.Socket.Family.INET (inaddrLOOPBACK)
-import Data.ByteString
+import System.Socket.Family.Inet as Inet
 import Data.Monoid
+import Data.ByteString
 import Control.Monad
 import Control.Concurrent
 import Control.Exception
 
 main :: IO ()
 main = do
-  s <- socket :: IO (Socket INET STREAM TCP)
-  setSockOpt s (SO_REUSEADDR True)
-  bind s (SockAddrIn 8080 inaddrLOOPBACK)
+  s <- socket :: IO (Socket Inet Stream TCP)
+  setSocketOption s (ReuseAddress True)
+  bind s addr
   listen s 5
   forever $ do
-    print "accept"
-    (peer,addr) <- accept s `onException` print "E01"
-    print "accepted"
-    print addr
-    sendAll peer "Hello world!" mempty 
-     `onException`
-       ( do print "E02"
-       )
-     `finally` 
-       ( do print "close"
-            close peer
-            print "closed"
-       )
-    print "end"
+    (peer,_) <- accept s
+    forkIO $ do
+      sendAll peer "Hello world!" mempty `finally` close peer
+  where
+    addr = SocketAddressInet Inet.loopback 8080
