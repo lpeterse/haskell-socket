8de1e916cca324b3f03b6c217f9306b4e4bbbb3d
Lars Petersen <info@lars-petersen.net>
2016-08-01 22:13:26 +0200

    #16: Added additional tests for connect. One reveals bug.

--- test/test.hs
+++ test/test.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Exception ( try, bracket, throwIO )
+import Control.Exception ( try, bracket, throwIO, catch )
 import Prelude hiding (head)
 
 import Test.Tasty
@@ -13,29 +13,88 @@ import System.Socket.Type.Stream
 import System.Socket.Protocol.TCP
 
 main :: IO ()
-main  = defaultMain $ testGroup "System" [ testGroup "Socket" [tgSocket]]
+main  = defaultMain $ testGroup "Tests" [ group01 ]
 
-tgSocket :: TestTree
-tgSocket = testGroup "connect"
+port :: InetPort
+port  = 39000
+
+group01 :: TestTree
+group01 = testGroup "Inet4/TCP"
   [ testCase "connect to closed port on inetLoopback" $ bracket
       ( socket :: IO (Socket Inet Stream TCP))
       close
       ( \s-> do
-          r <- try $ connect s (SocketAddressInet inetLoopback 39000)
+          r <- try $ connect s (SocketAddressInet inetLoopback port)
           case r of
             Left e   | e == eConnectionRefused -> return ()
                      | otherwise               -> throwIO e
             Right () -> assertFailure "connection should have failed"
       )
+
   , testCase "connect to closed port on inetNone" $ bracket
       ( socket :: IO (Socket Inet Stream TCP))
       close
       ( \s-> do
-          r <- try $ connect s (SocketAddressInet inetNone 64999)
+          r <- try $ connect s (SocketAddressInet inetNone port)
           case r of
             Left e   | e == eNetworkUnreachable  -> return ()
                      | e == eAddressNotAvailable -> return ()
                      | otherwise                 -> throwIO e
             Right () -> assertFailure "connection should have failed"
       )
+
+  , testCase "connect to open socket on localhost" $ bracket
+      ( do  server <- socket :: IO (Socket Inet Stream TCP)
+            client <- socket :: IO (Socket Inet Stream TCP)
+            return (server, client)
+      )
+      (\(server,client)-> do
+            close server
+            close client
+      )
+      (\(server,client)-> do
+            setSocketOption server (ReuseAddress True)
+            bind server (SocketAddressInet inetLoopback port)
+            listen server 5
+            connect client (SocketAddressInet inetLoopback port)
+      )
+
+  , testCase "connect closed socket" $ bracket
+      ( do  server <- socket :: IO (Socket Inet Stream TCP)
+            client <- socket :: IO (Socket Inet Stream TCP)
+            return (server, client)
+      )
+      (\(server,client)-> do
+            close server
+            close client
+      )
+      (\(server,client)-> do
+            setSocketOption server (ReuseAddress True)
+            bind server (SocketAddressInet inetLoopback port)
+            listen server 5
+            close client
+            connect client (SocketAddressInet inetLoopback port)
+      ) `catch` \e-> case e of
+          _ | e == eBadFileDescriptor -> return ()
+            | otherwise               -> assertFailure "expected eBadFileDescriptor"
+
+  , testCase "connect already connected socket" $ bracket
+      ( do  server <- socket :: IO (Socket Inet Stream TCP)
+            client <- socket :: IO (Socket Inet Stream TCP)
+            return (server, client)
+      )
+      (\(server,client)-> do
+            close server
+            close client
+      )
+      (\(server,client)-> do
+            setSocketOption server (ReuseAddress True)
+            bind server (SocketAddressInet inetLoopback port)
+            listen server 5
+            connect client (SocketAddressInet inetLoopback port)
+            connect client (SocketAddressInet inetLoopback port)
+            assertFailure "should have thrown eIsConnected"
+      ) `catch` \e-> case e of
+          _ | e == eIsConnected -> return ()
+            | otherwise         -> assertFailure "expected eIsConnected"
   ]
