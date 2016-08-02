d103657d595107e3a885a181333fb4411348cc8d
Lars Petersen <info@lars-petersen.net>
2016-08-02 20:55:30 +0200

    #16: Ported 'AddrInfo' test suite.

--- test/test.hs
+++ test/test.hs
@@ -2,7 +2,9 @@
 module Main where
 
 import Control.Exception ( try, bracket, throwIO, catch )
-import Prelude hiding (head)
+import Control.Monad ( when, void )
+import Prelude hiding ( head )
+import Data.Maybe ( isJust )
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -13,7 +15,7 @@ import System.Socket.Type.Stream
 import System.Socket.Protocol.TCP
 
 main :: IO ()
-main  = defaultMain $ testGroup "Tests" [ group01 ]
+main  = defaultMain $ testGroup "Tests" [ group01, group02 ]
 
 port :: InetPort
 port  = 39000
@@ -97,4 +99,26 @@ group01 = testGroup "Inet4/TCP"
       ) `catch` \e-> case e of
           _ | e == eIsConnected -> return ()
             | otherwise         -> assertFailure "expected eIsConnected"
+  ]
+
+group02 :: TestTree
+group02  = testGroup "getAddrInfo" [
+
+    testCase "getAddrInfo \"127.0.0.1\" \"80\"" $ do
+      ais <- getAddressInfo
+              (Just "127.0.0.1")
+              (Just "80")
+              aiNumericHost :: IO [AddressInfo Inet Stream TCP]
+      when (length ais /= 1) $ assertFailure "expected 1 result"
+      let [ai] = ais
+      when (isJust $ canonicalName ai) $ assertFailure "expected no canonical name"
+      let sa = socketAddress ai
+      when (inetAddress sa /= inetLoopback) $ assertFailure "expected loopback address"
+      when (inetPort    sa /= 80) $ assertFailure "expected port 80"
+
+  , testCase "getAddrInfo \"\" \"\"" $
+      void (getAddressInfo Nothing Nothing mempty :: IO [AddressInfo Inet Stream TCP]) `catch` \e-> case e of
+            _ | e == eaiNoName -> return ()
+            _                  -> assertFailure "expected eaiNoName"
+
   ]
