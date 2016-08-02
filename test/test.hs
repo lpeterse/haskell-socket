7ee7953ba1e9559daaa39d9c0707dc6120e4a593
Lars Petersen <info@lars-petersen.net>
2016-08-02 21:55:07 +0200

    #16: Ported 'IPV6ONLY' test suite.

--- test/test.hs
+++ test/test.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Control.Concurrent ( threadDelay )
+import Control.Concurrent.Async ( race )
 import Control.Exception ( try, bracket, throwIO, catch )
 import Control.Monad ( when, unless, void )
 import Prelude hiding ( head )
@@ -12,16 +13,20 @@ import Test.Tasty.HUnit
 
 import System.Socket
 import System.Socket.Family.Inet
+import System.Socket.Family.Inet6
 import System.Socket.Type.Stream
 import System.Socket.Type.Datagram
 import System.Socket.Protocol.TCP
 import System.Socket.Protocol.UDP
 
 main :: IO ()
-main  = defaultMain $ testGroup "Tests" [ group01, group02, group03, group99 ]
+main  = defaultMain $ testGroup "Tests" [ group01, group02, group03, group80, group99 ]
 
 port :: InetPort
 port  = 39000
+
+port6 :: Inet6Port
+port6 = 39000
 
 group01 :: TestTree
 group01 = testGroup "connect" [ testGroup "Inet/Stream/TCP" t1 ]
@@ -150,6 +155,55 @@ group03 = testGroup "send" [ testGroup "Inet/Stream/TCP"
           case e2 of
             Right _ -> assertFailure "expected ePipe"
             Left e  -> unless (e == ePipe) (throwIO e)
+        )
+    ]
+  ]
+
+group80 :: TestTree
+group80 = testGroup "setSocketOption" [ testGroup "V6Only"
+    [ testCase "present" $ bracket
+        ( do
+          server <- socket :: IO (Socket Inet6 Datagram UDP)
+          client <- socket :: IO (Socket Inet Datagram UDP)
+          return (server, client)
+        )
+        ( \(server,client)-> do
+          close server
+          close client
+        )
+        ( \(server,client)-> do
+          setSocketOption server (V6Only True)
+          bind server (SocketAddressInet6 inet6Any port6 0 0)
+          threadDelay 1000000 -- wait for the listening socket being set up
+          sendTo client "PING" mempty (SocketAddressInet inetLoopback port)
+          eith <- race
+            ( void $ receiveFrom server 4096 mempty )
+            ( threadDelay 1000000 )
+          case eith of
+            Left () -> assertFailure "expected timeout"
+            Right () -> return ()  -- timeout is the expected behaviour
+        )
+    , testCase "absent" $ bracket
+        ( do
+          server <- socket :: IO (Socket Inet6 Datagram UDP)
+          client <- socket :: IO (Socket Inet Datagram UDP)
+          return (server, client)
+        )
+        ( \(server,client)-> do
+          close server
+          close client
+        )
+        ( \(server,client)-> do
+          setSocketOption server (V6Only False)
+          bind server (SocketAddressInet6 inet6Any port6 0 0)
+          threadDelay 1000000 -- wait for the listening socket being set up
+          sendTo client "PING" mempty (SocketAddressInet inetLoopback port)
+          eith <- race
+            ( void $ receiveFrom server 4096 mempty )
+            ( threadDelay 1000000 )
+          case eith of
+            Left () -> return ()
+            Right () -> assertFailure "expected packet"
         )
     ]
   ]
