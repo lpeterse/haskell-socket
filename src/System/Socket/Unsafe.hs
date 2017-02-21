d98474d07380f73287c9b856270e02f18dd83f78
VyacheslavHashov <vyacheslavhashov@gmail.com>
2017-02-22 00:19:22 +0300

    Documentation about unsafe functions

--- src/System/Socket/Unsafe.hs
+++ src/System/Socket/Unsafe.hs
@@ -48,18 +48,30 @@ import System.Socket.Internal.Platform
 import System.Socket.Internal.Exception
 import System.Socket.Internal.Message
 
+-- | Like `System.Socket.send`, but using `Ptr` and length instead of a `ByteString`.
+--
+--   This function is /unsafe/. @bufPtr@ must refer to a buffer which size is at least @bufSize@ bytes.
 unsafeSend :: Socket a t p -> Ptr b -> CSize -> MessageFlags -> IO CInt
 unsafeSend s bufPtr bufSize flags =
   tryWaitRetryLoop s waitWrite (\fd-> c_send fd bufPtr bufSize flags )
 
+-- | Like `System.Socket.sendTo`, but using `Ptr` and length instead of a `ByteString`.
+--
+--   This function is /unsafe/. @bufPtr@ must refer to a buffer which size is at least @bufSize@ bytes.
 unsafeSendTo :: Socket f t p -> Ptr b -> CSize -> MessageFlags -> Ptr (SocketAddress f) -> CInt -> IO CInt
 unsafeSendTo s bufPtr bufSize flags addrPtr addrSize =
   tryWaitRetryLoop s waitWrite (\fd-> c_sendto fd bufPtr (fromIntegral bufSize) flags addrPtr addrSize)
 
+-- | Like `System.Socket.receive`, but using `Ptr` and length instead of a `ByteString`.
+--
+--   This function is /unsafe/. @bufPtr@ must refer to a buffer which size is at least @bufSize@ bytes.
 unsafeReceive :: Socket a t p -> Ptr b -> CSize -> MessageFlags -> IO CInt
 unsafeReceive s bufPtr bufSize flags =
   tryWaitRetryLoop s waitRead (\fd-> c_recv fd bufPtr bufSize flags)
 
+-- | Like `System.Socket.receiveFrom`, but using `Ptr` and length instead of a `ByteString`.
+--
+--   This function is /unsafe/. @bufPtr@ must refer to a buffer which size is at least @bufSize@ bytes.
 unsafeReceiveFrom :: Socket f t p -> Ptr b -> CSize -> MessageFlags -> Ptr (SocketAddress f) -> Ptr CInt -> IO CInt
 unsafeReceiveFrom s bufPtr bufSize flags addrPtr addrSizePtr =
   tryWaitRetryLoop s waitRead (\fd-> c_recvfrom fd bufPtr bufSize flags addrPtr addrSizePtr)
@@ -88,3 +100,4 @@ tryWaitRetryLoop s@(Socket mfd) wait action = loop 0
         Nothing -> do
           wait s iteration -- this call is (eventually) blocking
           loop $! iteration + 1 -- tail recursion
+
